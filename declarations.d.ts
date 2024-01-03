02f0f5d38ca092becc79d1031d5806bdadd6b63d
Kodai Aoyama <kodai.aoyama+github@gmail.com>
2024-01-03 15:10:17 +0900

    first commit

---
+++ declarations.d.ts
@@ -0,0 +1,6 @@
+declare module '@aquvee/wc'
+declare namespace JSX {
+    interface IntrinsicElements {
+      "aquvee-component": any;
+    }
+  }
---
+++ index.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import React, { useEffect } from 'react'
+
+type AquveeComponentProps = {
+  innerClass?: string
+  format?: string
+  query: string
+  aquveeUrl: string
+}
+
+export default function AquveeComponent(props: AquveeComponentProps) {
+  const { innerClass, format, query, aquveeUrl } = props;
+
+  useEffect(() => { import('@aquvee/wc'); return () => { }; }, [])
+
+  return (<aquvee-component
+            inner_class={innerClass}
+            format={format}
+            query={query}
+            aquvee_url={aquveeUrl} />)
+}
